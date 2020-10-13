e7fe5774e819598644a72db5d94324a8f9463272
Michal Leszczynski <michal.leszczynski@aptiv.com>
2020-10-13 11:26:04 +0200

    Refactor && Add config

--- insert_sensitive_data.sh
+++ insert_sensitive_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Cloning repo with secrets...'
-git clone git@github.com:MichalLeszczynski/secret_files.git
+git clone https://github.com/MichalLeszczynski/secret_files.git
 echo 'Copying secrets to desired places...'
 mv secret_files/hosts ansible/.
 mv secret_files/* ansible/files/common/.
